0f1a2798cb5e1cd2033dbd1065cb104f6a189e25
zrp0x0 <zrp0x0@gmail.com>
2026-04-15 11:12:17 +0900

    chore: 프로젝트 초기 환경 설정 및 의존성 추가 - Java 17, Spring Boot 3.5.13 기반 프로젝트 초기화 - JPA, H2, Lombok, MapStruct, Swagger 의존성 설정 - H2 로컬 데이터베이스 및 JPA 기본 설정 추가

---
+++ src/main/java/com/codeit/findex/P0FindexApplication.java
@@ -0,0 +1,13 @@
+package com.codeit.findex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P0FindexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P0FindexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeit/findex/P0FindexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeit.findex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P0FindexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
