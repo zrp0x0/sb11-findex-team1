2a10b3b22efb8985d74f6ae2b24093033fd33bf4
tndyd123 <wjdtndyd0001@gmail.com>
2026-04-15 16:17:39 +0900

    feat: 관심 지수 성과 조회 컨트롤러 구현

--- src/main/java/com/codeit/findex/domain/indexdata/repository/IndexDataRepository.java
+++ src/main/java/com/codeit/findex/domain/indexdata/repository/IndexDataRepository.java
@@ -2,10 +2,12 @@ package com.codeit.findex.domain.indexdata.repository;
 
 import com.codeit.findex.domain.indexdata.entity.IndexData;
 import java.util.List;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IndexDataRepository extends JpaRepository<IndexData, Long> {
 
   // 추후 주석 삭제 및, QueryDSL 적용예정
+  @EntityGraph(attributePaths = "indexInfo")
   List<IndexData> findByIndexInfo_FavoriteTrueOrderByIndexInfoIdAscBaseDateDesc();
 }
