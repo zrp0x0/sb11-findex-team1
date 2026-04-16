7f5fcbe2d48b779630ddfe199eb9a69755c475d2
성결 <ow00us@gmail.com>
2026-04-16 14:28:58 +0900

    docs: 검증 메시지 추가 (#16)

--- src/main/java/com/codeit/findex/domain/autosyncconfig/dto/AutoSyncConfigUpdateRequest.java
+++ src/main/java/com/codeit/findex/domain/autosyncconfig/dto/AutoSyncConfigUpdateRequest.java
@@ -3,7 +3,7 @@ package com.codeit.findex.domain.autosyncconfig.dto;
 import jakarta.validation.constraints.NotNull;
 
 public record AutoSyncConfigUpdateRequest(
-    @NotNull
+    @NotNull(message = "활성화 여부는 필수입니다.")
     Boolean enabled
 ) {
 
