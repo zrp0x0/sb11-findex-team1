686ea7e3554429e58de634c626162089b4158feb
tndyd123 <wjdtndyd0001@gmail.com>
2026-04-17 18:33:22 +0900

    feat: 지수 정보 연동 컨트롤러 구현 및 202 응답 처리

--- src/main/java/com/codeit/findex/domain/syncjob/controller/SyncJobController.java
+++ src/main/java/com/codeit/findex/domain/syncjob/controller/SyncJobController.java
@@ -2,23 +2,41 @@ package com.codeit.findex.domain.syncjob.controller;
 
 import com.codeit.findex.domain.syncjob.dto.indexinfo.IndexInfoSyncJobResponse;
 import com.codeit.findex.domain.syncjob.service.SyncJobService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@Tag(name = "연동 작업 API", description = "연동 작업 관리 API")
+@RestController
 @RequestMapping("/api/sync-jobs")
 @RequiredArgsConstructor
 public class SyncJobController {
 
   private final SyncJobService syncJobService;
 
+  @Operation(
+      summary = "지수 정보 연동",
+      description = "Open API를 통해 지수 정보를 연동합니다.",
+      operationId = "syncIndexInfos")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "202", description = "연동 작업 생성 성공"),
+      @ApiResponse(responseCode = "400", description = "잘못된 요청"),
+      @ApiResponse(responseCode = "500", description = "서버 오류")
+  })
   @PostMapping("/index-infos")
-  public ResponseEntity<List<IndexInfoSyncJobResponse>> syncIndexInfos() {
-    return ResponseEntity.ok(syncJobService.syncIndexInfos("worker1"));
+  public ResponseEntity<List<IndexInfoSyncJobResponse>> syncIndexInfos(HttpServletRequest request) {
+    // getRemoteAddr - ip 주소 가져오기
+    String worker = request.getRemoteAddr();
+    List<IndexInfoSyncJobResponse> response = syncJobService.syncIndexInfos(worker);
+    return ResponseEntity.status(HttpStatus.ACCEPTED).body(response);
   }
-
 }
