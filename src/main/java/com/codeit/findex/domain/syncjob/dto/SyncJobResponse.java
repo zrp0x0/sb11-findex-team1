276ac1b904bd8a78d921db368acb9cd7fcadfeef
성결 <ow00us@gmail.com>
2026-04-17 11:46:25 +0900

    feat: 연동 작업 조회 DTO 및 검증 조건 작성 (#25)

---
+++ src/main/java/com/codeit/findex/domain/syncjob/dto/SyncJobResponse.java
@@ -0,0 +1,18 @@
+package com.codeit.findex.domain.syncjob.dto;
+
+import com.codeit.findex.domain.common.enums.JobType;
+import com.codeit.findex.domain.common.enums.SyncResult;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public record SyncJobResponse(
+    Long id,
+    JobType JobType,
+    Long indexInfold,
+    LocalDate targetDate,
+    String worker,
+    LocalDateTime jobTime,
+    SyncResult result
+) {
+
+}
